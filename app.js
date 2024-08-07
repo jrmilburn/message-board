fce097c2936db68a5eef91aa1dc30f4f25465e46
Joe Milburn <jrmilburn@outlook.com>
2024-08-07 13:22:31 +1000

    Add message post and view functionality

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const path = require('node:path');
 const express = require('express');
 const app = express();
+const newRouter = require("./routes/newRouter");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/new", newRouter);
 
 const PORT = 3000;
 
 const messages = [
     {
-      text: "Hi there!",
-      user: "Amando",
+      text: "Bro doin?",
+      user: "Dylan",
       added: new Date()
     },
     {
-      text: "Hello World!",
-      user: "Charles",
+      text: "Just codin",
+      user: "Joe",
       added: new Date()
-    }
+    },
+    {
+        text: "You?",
+        user: "Joe",
+        added: new Date()
+    },
+    {
+        text: "Calling a coder",
+        user: "Dylan",
+        added: new Date()
+    },
   ];
 
 app.get('/', (req, res) => {
@@ -25,10 +40,28 @@ app.get('/', (req, res) => {
 
 });
 
-app.get('/new', (req, res) => {
+app.post('/new', (req, res) => {
 
-    res.render('new');
+    const messageUser = req.body.name;
+    const messageText = req.body.message;
+
+    messages.push({ text: messageText, 
+                    user: messageUser, 
+                    added: new Date() });
+
+    res.redirect('/');
 
 });
+
+app.get('/message/:index', (req, res) => {
+
+    const message = messages[req.params.index];
+    if(message) {
+        res.render('message', { message: message });
+    } else {
+        res.status(404).send('Message not found');
+    }
+
+})
 
 app.listen(PORT, () => console.log('app started'));
