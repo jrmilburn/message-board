3cb2b807e64d49f9f434c16550e5663615beb6a1
Joe Milburn <jrmilburn@outlook.com>
2024-08-10 20:35:32 +1000

    add database interaction functions

---
+++ db/queries.js
@@ -0,0 +1,17 @@
+const pool = require("./pool");
+
+async function getAllMessages() {
+    
+}
+
+async function addMessage()  {
+
+}
+
+async function editMessage() {
+
+}
+
+async function deleteMessage() {
+
+}
